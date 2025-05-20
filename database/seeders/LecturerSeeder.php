fe6a5421f7d51ed519994f8ff086ca544b9eefb1
Wildan <wildanwigenta52@gmail.com>
2025-05-20 13:39:08 +0700

    add lecture seeder

---
+++ database/seeders/LecturerSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LecturerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+{
+    \App\Models\Lecturer::insert([
+        ['name' => 'Dr. Budi', 'nidn' => '1234567890'],
+        ['name' => 'Prof. Ani', 'nidn' => '0987654321'],
+    ]);
+}
+
+}
