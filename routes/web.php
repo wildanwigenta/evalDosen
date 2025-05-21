ca0d159a00751c0a77adf7ed094f7780b2890320
Wildan <wildanwigenta52@gmail.com>
2025-05-21 23:41:37 +0700

    add route mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware(['auth', 'role:mahasiswa'])->prefix('mahasiswa')->group(function () {
+    Route::get('/dashboard', [MahasiswaController::class, 'dashboard'])->name('mahasiswa.dashboard');
+    Route::get('/evaluasi/{course}', [MahasiswaController::class, 'formEvaluasi'])->name('mahasiswa.evaluasi');
+    Route::post('/evaluasi/{course}', [MahasiswaController::class, 'submitEvaluasi']);
+});
