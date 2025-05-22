da026e48cf0a357469a6d8020f128685862a0084
Wildan <wildanwigenta52@gmail.com>
2025-05-22 21:02:47 +0700

    clear evaluation model

--- app/Models/Evaluation.php
+++ app/Models/Evaluation.php
@@ -14,12 +14,10 @@ class Evaluation extends Model
     {
         return $this->hasMany(EvaluationAnswer::class);
     }
-
     public function course()
     {
         return $this->belongsTo(Course::class);
     }
-
     public function user()
     {
         return $this->belongsTo(User::class);
