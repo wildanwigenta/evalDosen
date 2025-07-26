48b6d6f08f30899668c205c7d51e850cffabec98
Wildan <wildanwigenta52@gmail.com>
2025-07-26 19:55:29 +0700

    updated

--- app/Filament/Resources/LecturerResource.php
+++ app/Filament/Resources/LecturerResource.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class LecturerResource extends Resource
+
 {
     protected static ?string $model = Lecturer::class;
     protected static ?string $navigationIcon = 'heroicon-s-rectangle-stack';
